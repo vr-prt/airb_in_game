2b2fe30bd81ee85065626c40fcfc81bce7ab64f1
Victor Prete <vprete@tuta.io>
2022-08-15 16:45:20 -0400

    Added game world model

---
+++ db/migrate/20220815204105_create_game_worlds.rb
@@ -0,0 +1,11 @@
+class CreateGameWorlds < ActiveRecord::Migration[7.0]
+  def change
+    create_table :game_worlds do |t|
+      t.string :name
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
