a8d404ac5aa8f1138ab1d52c65903d255110f76f
Victor Prete <vprete@tuta.io>
2022-08-15 17:22:37 -0400

    Added new method to game worlds controller

--- app/controllers/game_worlds_controller.rb
+++ app/controllers/game_worlds_controller.rb
@@ -1,4 +1,20 @@
 class GameWorldsController < ApplicationController
   def index
   end
+
+  def new
+    @game_world = GameWorld.new
+  end
+
+  def create
+    @user = current_user
+    @game_world = GameWorld.new(game_world_params)
+    @game_world.user = @user
+  end
+
+  private
+
+  def game_world_params
+    params.require(:game_world).permit(:name, :description)
+  end
 end
