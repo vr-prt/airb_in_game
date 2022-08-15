08d1aff5a4e4e4cbd91334b7dfffe08537a741a9
Victor Prete <vprete@tuta.io>
2022-08-15 16:50:57 -0400

    Genereated game worlds controller

---
+++ app/controllers/game_worlds_controller.rb
@@ -0,0 +1,4 @@
+class GameWorldsController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/game_worlds_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GameWorldsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
