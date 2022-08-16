b8ef15e3ef69ff3c2e811ab0de92eda89382d104
Mikael Gonsalves <mjgonsalves@protonmail.com>
2022-08-16 14:54:59 -0400

    controller reservations created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :game_worlds, :reservations
+  has_many :game_worlds
+  has_many :reservations
 end
