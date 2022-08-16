6e5544779da724322f77993317bc36c413e7095b
Mikael Gonsalves <mjgonsalves@protonmail.com>
2022-08-16 12:04:15 -0400

    seed user gameworld creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Creating 20 fake users..."
+
+20.times do
+ user = User.create(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+  )
+
+  game_world = GameWorld.new(
+    name: Fake::Game.title,
+    description: Faker::Quote.yoda,
+   )
+
+  game_world.user = user
+  game_world.save!
+end
+
+# puts "20 fake users created successfully"
+
+# puts "Creating 10 Game Worlds"
+
+# puts "20 fake game worlds created successfully"
+
+# puts "Seeds finished! Goodbye"
